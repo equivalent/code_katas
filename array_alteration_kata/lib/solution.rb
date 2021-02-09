3fa4e42a55de198b678ae23760351c9630e266f8
Tomas Valent <equivalent@eq8.eu>
2021-02-09 21:42:24 +0100

    pass the simple example

--- array_alteration_kata/lib/solution.rb
+++ array_alteration_kata/lib/solution.rb
@@ -1,7 +1,13 @@
+require 'irb'
 module Solution
   extend self
 
   def solution(a,b)
-    []
+    a.each_with_index do |a_item, index|
+      b.each do |b_item|
+        a.insert(index + 1, b_item) if b_item > a_item
+      end
+    end
+    a
   end
 end
