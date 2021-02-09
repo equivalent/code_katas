9b179a72c5d38e561da797fbc8726f6da179d832
Tomas Valent <equivalent@eq8.eu>
2021-02-09 21:35:20 +0100

    define full and basic examples to solve

--- array_alteration_kata/lib/solution.rb
+++ array_alteration_kata/lib/solution.rb
@@ -2,6 +2,6 @@ module Solution
   extend self
 
   def solution(a,b)
-    false
+    []
   end
 end
--- array_alteration_kata/spec/solution_spec.rb
+++ array_alteration_kata/spec/solution_spec.rb
@@ -2,10 +2,20 @@ require 'spec_helper'
 
 RSpec.describe Solution do
   let(:result) { described_class.solution(a, b)  }
-  let(:a) {  }
-  let(:b) {  }
 
-  it do
-    expect(result).to be true
+  context 'basic example' do
+    let(:a) { [1,3] }
+    let(:b) { [2] }
+    it do
+      expect(result).to eq([1,2,3])
+    end
+  end
+
+  xcontext 'full example' do
+    let(:a) { [1,3,5] }
+    let(:b) { [2, 4, 8] }
+    it do
+      expect(result).to eq([1,2,3,4,5,8])
+    end
   end
 end
