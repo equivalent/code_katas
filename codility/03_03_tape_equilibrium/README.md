279d515a71a7ebb6fac4ce627660daacd2232b2e
Tomas Valent <equivalent@eq8.eu>
2021-02-17 21:43:05 +0100

    park problem

--- codility/03_03_tape_equilibrium/README.md
+++ codility/03_03_tape_equilibrium/README.md
@@ -6,6 +6,48 @@
 
 
 ```
+A - non-empty arary consist of N integers
+
+0 < P < N
+
+P splits tape into two non-empty parts:
+  A[0], A[1]   .. A[P-1]
+  A[P], A[P+1] .. A[N-1]
+
+difference between this two partts is a value 
+
+  (A[0] + A[1] + ..+ A[P-1] ) - (A[P] + A[P+1] + ..+ A[N-1])
+
+(absolute difference)
+
+* efficient
+* N  [2..100_000]
+* element in A is integer in range -1_000..1_000
+
+example
+
+A = [3, 1, 2 , 4, 3]
+
+if P = 1
+  [[3], [1, 2 , 4, 3]]
+  abs(3 - 10) = 7
+
+if P = 2
+  [[3, 1], [2 , 4, 3]]
+  abs(4 - 9) = 5
+
+if P = 3
+  [[3, 1, 2], [4, 3]]
+  abs(6 - 7) = 1
+
+
+if P = 4
+  [[3, 1, 2, 4], [3]]
+  abs(10 - 3) = 7
+
+
+solution is 1 as that's the minimal difference that can be achived 
+
 ```
 
 ## Solution
--- codility/03_03_tape_equilibrium/spec/solution_spec.rb
+++ codility/03_03_tape_equilibrium/spec/solution_spec.rb
@@ -4,6 +4,19 @@ module Solution
   extend self
 
   def solution(a)
+    # 0 < P < a.size
+
+    minimum_division = nil
+    (1..a.size-1).each do |pp|
+      part1 = a[0..(pp-1)]
+
+      part2 = a[pp..(a.size-1)]
+
+      abs_division = (part1.inject(:+).abs - part2.inject(:+).abs).abs
+      minimum_division ||= abs_division
+      minimum_division = abs_division if abs_division < minimum_division
+    end
+    minimum_division
   end
 end
 
@@ -11,8 +24,48 @@ RSpec.describe Solution do
   let(:res) { described_class.solution(a) }
 
   context do
-    let(:a) {  }
+    let(:a) { [3, 1, 2, 4, 3] }
+    it { expect(res).to eq 1 }
+  end
 
-    it { expect(res).to eq :todo }
+  # my examples
+  context do
+    let(:a) { (1..5).to_a }
+    it { expect(res).to eq 3 }
+  end
+
+  context do
+    let(:a) { [-3,-2,-1] }
+    it { expect(res).to eq 0 }
+  end
+
+  context do
+    let(:a) { [-4, -3,-2,-1] }
+    it { expect(res).to eq 2 }
+  end
+
+  context do
+    let(:a) { [-8, 2, 3] }
+    it { expect(res).to eq 3 }
+  end
+
+  context do
+    let(:a) { [-10, -8, 2, 3] }
+    it { expect(res).to eq 7 }
+  end
+
+  context do
+    let(:a) { [1,2] }
+    it { expect(res).to eq 1 }
+  end
+
+  context do
+    let(:a) { [1,1] }
+    it { expect(res).to eq 0 }
+  end
+
+  context do
+    let(:a) { (-1000..1000).to_a }
+    it { expect(res).to eq 0 }
   end
 end
